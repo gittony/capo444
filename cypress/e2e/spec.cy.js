dcfe4e2f51c21f51bde63495076a6b91e8bccdab
Tony White <tonywhite@gmail.com>
2022-09-01 19:23:07 -0500

    copy edit to h1

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('/')
-    cy.get('h1').should('contain.text', 'Hello').should('be.visible')
+    cy.get('h1').should('contain.text', 'Hello again').should('be.visible')
   })
 })
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
       <div>
-        <h1>Hello</h1>
+        <h1>Hello again</h1>
         <p>How are you</p>
       </div>
     )
